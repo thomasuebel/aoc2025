9de855f2a54b1a30634b315dba0cb39b26348d13
Thomas Uebel <thomas.uebel@misterspex.de>
2025-12-04 21:54:12 +0100

    add the day2 id object

---
+++ src/main/java/aoc2025/day2/ID.java
@@ -0,0 +1,13 @@
+package aoc2025.day2;
+
+public class ID {
+    String id;
+
+    public ID(String id) {
+        this.id = id;
+    }
+
+    public boolean isValid() {
+        return true;
+    }
+}
