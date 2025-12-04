a33a9455b52f16bc7962c6e67c4ebe9cee1f3254
Thomas Uebel <thomas.uebel@misterspex.de>
2025-12-04 22:03:19 +0100

    add test that defines a string as invalid

--- src/test/java/aoc2025/day2/IDTest.java
+++ src/test/java/aoc2025/day2/IDTest.java
@@ -1,6 +1,8 @@
 package aoc2025.day2;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class IDTest {
@@ -8,5 +10,11 @@ public class IDTest {
     void id_is_valid_string() {
         ID id = new ID("12");
         assertTrue(id.isValid());
+    }
+
+    @Test
+    void is_invalid_string() {
+        ID id = new ID("11");
+        assertFalse(id.isValid());
     }
 }
