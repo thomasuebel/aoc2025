85ce010f0f40fbad6b26d6f2ccb556a054c6e1c7
Thomas Uebel <thomas.uebel@misterspex.de>
2025-12-04 22:20:23 +0100

    identify an invalid id using backref regex

--- src/main/java/aoc2025/day2/ID.java
+++ src/main/java/aoc2025/day2/ID.java
@@ -3,6 +3,9 @@ package aoc2025.day2;
 public class ID {
     String id;
 
+    // regular expression for invalid ids - i.e. ids that are made only of some sequence of digits repeated twice
+    private static final String A_SINGLE_DIGIT_REPEATED_ONCE = "(\\d)\\1";
+
     public ID(String id) {
         this.id = id;
     }
@@ -15,7 +18,7 @@ public class ID {
     // which is made only of some sequence of digits repeated twice. So, 55 (5 twice), 6464 (64 twice), and
     // 123123 (123 twice) would all be invalid IDs.
     public boolean isInvalid() {
-        return false;
+        return id.matches(A_SINGLE_DIGIT_REPEATED_ONCE);
     }
 
 }
