166146d9964a3f535a81bc9daaefe11b45fdecde
Thomas Uebel <thomas.uebel@misterspex.de>
2025-12-04 21:53:49 +0100

    add a test for an arbitrary day2 id

--- src/test/java/aoc2025/day2/IDTest.java
+++ src/test/java/aoc2025/day2/IDTest.java
@@ -1,4 +1,12 @@
 package aoc2025.day2;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class IDTest {
+    @Test
+    void id_is_valid_string() {
+        ID id = new ID("12");
+        assertTrue(id.isValid());
+    }
 }
